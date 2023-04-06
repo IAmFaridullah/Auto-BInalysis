5479b17670ea813c8f716b34564a315d53bc74a4
Farid Ullah <faridullahkhan645@gmail.com>
2023-04-07 01:21:38 +0500

    changes made

--- Auto_Binalysis_Backend/analysis/models.py
+++ Auto_Binalysis_Backend/analysis/models.py
@@ -5,18 +5,19 @@ import uuid
 # from django.db import models
 from django.db import models
 
+
 class TrainedModel(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)    
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     model_name = models.CharField(max_length=255)
     username = models.CharField(max_length=255)
-    model_path = models.CharField(max_length=255,default=' ')
+    model_path = models.CharField(max_length=255, default=' ')
     model_for = models.CharField(max_length=255)
     accuracy = models.FloatField()
     rmse = models.FloatField()
-    
+
     class Meta:
         db_table = 'trained_models'
 
     def __str__(self):
         return self.model_name
-
